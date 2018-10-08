17cfdbbce92d8751b0d0bacb46a134383b76b15d
Ha <haquanghuy@mail.ru>
2018-10-08 22:29:32 +0300

    lesson2

---
+++ script.js
@@ -0,0 +1,51 @@
+let money = +prompt("Ваш бюджет на месяц?",''),
+    time = prompt("Введите дату в формате YYYY-MM-DD",'');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+/*for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');
+    
+    if ((typeof(a))==="string" && (typeof(a))!= null && (typeof(b))!= null
+        && a != '' && b != '' && a.length < 50){
+            console.log("okey");
+            appData.expenses[a] = b;
+        }
+        else{
+            i--;
+        }
+}*/
+let i = 0;
+while ( i < 2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');
+    if ((typeof(a))==="string" && (typeof(a))!= null && (typeof(b))!= null
+        && a != '' && b != '' && a.length < 50){
+            console.log("okey");
+            appData.expenses[a] = b;
+        }
+        else{
+            i--;
+        }
+    i++;
+}
+
+appData.moneyPerDay = appData.budget / 30;
+alert("бюджет на 1 день: "+ appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log("Мин ур достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Сред ур достатка");
+} else if (appData.moneyPerDay > 2000){
+    console.log("Высокий ур достатка");
+} else console.log("Ошибка");
+
